88e739fe778492ae3d95c1d78af26399f6ccab76
unknown <hossamfathi050@outlook.com>
2023-02-24 17:26:46 +0300

    first example

---
+++ asd/index.php
@@ -0,0 +1,71 @@
+
+<?php 
+echo 'q1===========<br/>'; 
+echo 'Hello<br/>'; 
+echo 'Ahmed Ali<br/>'; 
+
+
+
+echo 'q2===========<br/>'; 
+echo '5 + 8 * 6 = ';
+echo 5 + 8 * 6;
+echo'<br>'; 
+
+echo '(55+9) % 9 = ' ;
+echo (55+9) % 9 ;
+echo'<br>';
+
+echo '20 + -3*5 / 8 = ' ;
+echo 20 + -3*5 / 8 ;
+echo'<br>';
+
+echo '5 + 15 / 3 * 2 - 8 % 3 = ' ;
+echo 5 + 15 / 3 * 2 - 8 % 3 ;
+echo'<br>';
+
+
+echo 'q3===========<br/>'; 
+
+
+echo '((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) = ' ;
+echo ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) ;
+echo'<br>';
+
+
+echo 'q6===========<br/>'; 
+
+
+echo $username = "   Ahmed Muhammad "; 
+echo'<br>';
+
+echo trim($username);
+ 
+echo'<br>';
+
+
+echo 'q7===========<br/>'; 
+
+$num = 15023198.91; 
+
+echo number_format( $num );
+
+echo'<br>';
+
+echo 'q10===========<br/>'; 
+echo octdec('10') ;
+echo'<br>';
+
+
+
+echo 'q11===========<br/>'; 
+
+for ($i=1; $i<=99; $i+=2)
+	
+{
+  	  
+echo $i.'<br/>';
+	   
+} 
+echo'<br>';
+
+?>
