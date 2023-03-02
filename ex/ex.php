4fa9eb681cd5238d77f9d06fc1491a73d206b30a
unknown <hossamfathi050@outlook.com>
2023-03-02 20:25:47 +0300

    first example

---
+++ ex/ex.php
@@ -0,0 +1,32 @@
+ 
+<?php
+ 
+
+$x=55377700;
+echo $x;
+echo '<br>';
+echo gettype ($x);
+echo '<br>';
+echo '<br>';
+
+echo $x;
+echo "<br>";
+echo "=============";
+
+echo "<br>";
+for($x=1;$x<15;$x+=2)
+{
+echo $x;
+echo "<br>";
+}
+
+echo "<br>";
+
+function test($name){
+    echo "hossam dev." ;
+echo "Hello $name";
+
+}
+test("SAMA HOSSSAM FATHI");
+ 
+?>
